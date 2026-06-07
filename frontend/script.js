11a3fc7549bb15a7d51e258b343570f2283e6e78
deeyapatil26 <deeyapatil26@gmail.com>
2026-06-06 22:58:22 -0700

    script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,33 @@
+function login() {
 
+    let username =
+        document.getElementById("username").value;
+
+    let password =
+        document.getElementById("password").value;
+
+    if(username === "student" &&
+       password === "1234")
+    {
+        alert("Login Successful");
+    }
+    else{
+        alert("Invalid Credentials");
+    }
+}
+
+function applyLeave() {
+
+    let reason =
+        document.getElementById("leaveReason").value;
+
+    if(reason.trim() === "")
+    {
+        alert("Please enter reason");
+        return;
+    }
+
+    document.getElementById("leaveMessage")
+    .innerText =
+    "Leave Application Submitted Successfully!";
+}
